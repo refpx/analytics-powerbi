321d127c8e5f50f554e709f9d13ce5ef4c54e188
Fredy Romero <romeroespinoza.fp@gmail.com>
2024-06-17 11:45:18 -0500

    add styles to table

--- api/index.php
+++ api/index.php
@@ -2,7 +2,6 @@
 
 <head>
   <title>Analytics / Power BI</title>
-  <description>Report in Alma/Analytics to export some report</description>
 </head>
 
 <body>
@@ -39,10 +38,18 @@
       }
     }
 
+    // Estilos básicos para la tabla
+    echo '<style>';
+    echo 'table { border-collapse: collapse; width: 100%; }';
+    echo 'th, td { text-align: left; padding: 8px; }';
+    echo 'tr:nth-child(even) { background-color: #f2f2f2; }';
+    echo 'th { background-color: #4CAF50; color: white; }';
+    echo '</style>';
+
     echo '<table>';
 
     foreach ($rowset->Row as $row_number) {
-      echo '<tr><td>' . $row_number[0]->Column1 . ' </td><td>' . $row_number[0]->Column2 . '</td><td>' . $row_number[0]->Column3 . ' </td><td>' . $row_number[0]->Column4 . ' </td></tr>';
+      echo '<tr><td>' . $row_number[0]->Column1 . ' </td><td>' . $row_number[0]->Column2 . '</td><td>' . $row_number[0]->Column3 . ' </td><td>' . $row_number[0]->Column4 . ' </td><td>' . $row_number[0]->Column5 . ' </td><td>' . $row_number[0]->Column6 . ' </td><td>' . $row_number[0]->Column7 . ' </td><td>' . $row_number[0]->Column8 . ' </td><td>' . $row_number[0]->Column9 . ' </td><td>' . $row_number[0]->Column10 . ' </td><td>' . $row_number[0]->Column11 . ' </td><td>' . $row_number[0]->Column12 . ' </td></tr>';
     }
     echo '</table>';
 
